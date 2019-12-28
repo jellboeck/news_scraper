6a6aae407ac5a84c83301de49511becb1a98af78
Julia Ellboeck <juliaellboeck@gmail.com>
2019-12-28 11:05:45 -0500

    set up server and file structure

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var expressHandlebars = require("express-handlebars");
+var mongoose = require("mongoose");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+var router = express.Router();
+
+app.use(express.static(__dirname + "/public"));
+
+app.engine("handlebars", expressHandlebars({
+    defaultLayout: "main"
+}));
+
+app.set("view engine", "handlebars");
+
+app.use(express.urlencoded({
+    extended: false
+}));
+
+app.use(router);
+
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(db, function (error){
+    if (error){
+        console.log(error);
+    }
+    console.log("mongoose connection successful")
+})
+
+app.listen(PORT, function(){
+    console.log("listening on port: " + PORT);
+});
+
+
+
+
